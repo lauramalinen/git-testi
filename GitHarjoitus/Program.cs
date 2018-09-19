88d2e2b766398afd14f4c46609d4830b4012dd19
lauramalinen <laura.malinen@gmail.com>
2018-09-19 11:47:31 +0300

    msg

--- GitHarjoitus/Program.cs
+++ GitHarjoitus/Program.cs
@@ -10,6 +10,8 @@ namespace GitHarjoitus
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello world!");
+            Console.ReadKey();
         }
     }
 }
